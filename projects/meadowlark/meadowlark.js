627d462b2f0dc3286a8ac87c726cf234410cfe4c
William M <wmooney1984@gmail.com>
2020-01-13 16:31:16 -0500

    Handlebars Added

--- projects/meadowlark/meadowlark.js
+++ projects/meadowlark/meadowlark.js
@@ -1,35 +1,41 @@
 const express = require('express');
+const expressHandlebars = require('express-handlebars');
+
 const app = express();
+
+// configure Handlebars view engine
+app.engine(
+  'handlebars',
+  expressHandlebars({
+    defaultLayout: 'main'
+  })
+);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.type('text/plain');
-  res.send('Meadowlark Travel');
-});
+app.get('/', (req, res) => res.render('home'));
 
-app.get('/about', (req, res) => {
-  res.type('text/plain');
-  res.send('About Meadowlark Travel');
-});
+app.get('/about', (req, res) => res.render('about'));
 
-// Custom 404 page
+// custom 404 page
 app.use((req, res) => {
-  res.type('text/plain');
   res.status(404);
-  res.send('404 - Not Found');
+  res.render('404');
 });
 
-// Custom 500 page
+// custom 500 page
 app.use((err, req, res, next) => {
-  console.log(err.message);
-  res.type('text/plain');
+  console.error(err.message);
   res.status(500);
-  res.send('500 - Server Error');
+  res.render('500');
 });
 
 app.listen(port, () =>
   console.log(
     `Express started on http://localhost:${port}; ` +
-      'press Ctrl+C to terminate.'
+      `press Ctrl-C to terminate.`
   )
 );
